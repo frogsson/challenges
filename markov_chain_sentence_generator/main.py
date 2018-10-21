2106be93513437e20cd0c3c8ba12e0436fe18878
kim <taengoe@gmail.com>
2018-10-21 04:02:45 +0200

    init

---
+++ markov_chain_sentence_generator/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+#   word      next   probability
+# {"hello": {"word": 100%}}
+def feed(text):
+    pass
+
+def generate_sentence():
+    pass
+
+def main():
+    print("main")
+
+if __name__ == "__main__":
+    main()
