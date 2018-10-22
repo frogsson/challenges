51b8797003239652db816d53ddbd900e2c2cca39
kim <taengoe@gmail.com>
2018-10-22 03:15:37 +0200

    start on a dictory generator

--- markov_chain_sentence_generator/main.py
+++ markov_chain_sentence_generator/main.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
+import json
 
 
 #   word      next   probability
 # {"hello": {"word": 100%}}
-def feed(text):
-    pass
+def generate_dict(text, save_as):
+    with open(text, "r") as f:
+        print(f.read())
 
 def generate_sentence():
     pass
 
 def main():
     print("main")
+    generate_dict("generic.txt")
 
 if __name__ == "__main__":
     main()
